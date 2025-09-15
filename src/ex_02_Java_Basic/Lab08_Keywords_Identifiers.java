e499d34acfc13ea064da054c7f90bd508af0e698
Siva <siva.alluris@gmail.com>
2025-09-15 12:05:59 +0530

    KeyIdentifiers

---
+++ src/ex_02_Java_Basic/Lab08_Keywords_Identifiers.java
@@ -0,0 +1,8 @@
+package ex_02_Java_Basic;
+
+public class Lab08_Keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
