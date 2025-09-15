1fb8073f2be8e95397af38ba73b92ea20f02764e
Siva <siva.alluris@gmail.com>
2025-09-15 14:21:42 +0530

    Common_Math

---
+++ src/ex_02_Java_Basic/Lab11_Common_Maths.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basic;
+
+public class Lab11_Common_Maths {
+    public static void main(String[] args) {
+
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2/2);
+        System.out.println(2-2);
+
+        System.out.println("2+2");
+        // the string which is in "2 +2" will print as it is.
+
+        System.out.println(3/2); // 3/2 =1.5 but Integer part --> No decimal
+
+
+    }
+}
