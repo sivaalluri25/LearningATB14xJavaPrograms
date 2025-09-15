8ef5918b15e6dbf2fb6487dbb45954781fe1862b
Siva <siva.alluris@gmail.com>
2025-09-15 14:33:10 +0530

    Math_Exception

---
+++ src/ex_02_Java_Basic/Lab12_Commom_Math_Exception.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basic;
+
+public class Lab12_Commom_Math_Exception
+{
+    public static void main(String[] args) {
+        System.out.println("10/0");
+        System.out.println("3/0.5");
+        // Exception in thread "main" java.lang.ArithameticException
+    }
+}
