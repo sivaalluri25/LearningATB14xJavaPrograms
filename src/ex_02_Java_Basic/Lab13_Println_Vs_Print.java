5c2abe84498051b2583cede946cc58bec793d169
Siva <siva.alluris@gmail.com>
2025-09-15 14:30:32 +0530

    Common_Math

---
+++ src/ex_02_Java_Basic/Lab13_Println_Vs_Print.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basic;
+
+public class Lab13_Println_Vs_Print
+{
+    public static void main(String[] args) {
+        /*System.out.println("Hi");
+        System.out.println("BYE"); */ // ln ----> newline it will show
+
+        System.out.println("Hi");
+        System.out.println("BYE");
+    }
+}
