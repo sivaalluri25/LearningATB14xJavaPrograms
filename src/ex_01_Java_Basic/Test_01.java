e13263e13d73e45bc979b196aadb5d1c445e27af
Siva <siva.alluris@gmail.com>
2025-09-15 11:29:50 +0530

    Hello 1234

--- src/ex_01_Java_Basic/Test_01.java
+++ src/ex_01_Java_Basic/Test_01.java
@@ -1,4 +1,10 @@
 package ex_01_Java_Basic;
 
 public class Test_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 4");
+    }
 }
