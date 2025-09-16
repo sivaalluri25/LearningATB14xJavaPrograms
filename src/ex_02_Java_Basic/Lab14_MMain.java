5e93d4df3a98bbb7489978e0e085e513bbb7d8f8
Siva <siva.alluris@gmail.com>
2025-09-16 12:37:21 +0530

    Main

---
+++ src/ex_02_Java_Basic/Lab14_MMain.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Basic;
+
+public class Lab14_MMain {
+    public static void main(String[] args) { // only one Main Method  will be.
+
+    }
+
+    public static void main(String args) { // Different parameters []
+
+    }
+
+    public static void main(int args) {
+
+// There can be multiple main Methods, but only one main method with JVM recognize
+        // the concept of multiple main function in a program is called as main Method Overloading
+    }
+}
