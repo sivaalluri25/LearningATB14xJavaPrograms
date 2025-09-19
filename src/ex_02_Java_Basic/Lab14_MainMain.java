842ac7f81d5fab57fe947d6222f2be99602d9dd7
Siva <siva.alluris@gmail.com>
2025-09-19 13:23:31 +0530

    Value of a & b

--- src/ex_02_Java_Basic/Lab14_MainMain.java
+++ src/ex_02_Java_Basic/Lab14_MainMain.java
@@ -2,6 +2,6 @@ package ex_02_Java_Basic;
 
 public class Lab14_MainMain {
     public static void main(String[] args) {
-        System.out.println(10+3/2-1*5); //need to use Bodmas Rule
+        System.out.println(10+3/2-1*5); //Need to use Bodmas Rule
     }
 }
